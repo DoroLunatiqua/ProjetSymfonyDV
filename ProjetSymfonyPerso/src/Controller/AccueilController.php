35e879cd4adebed914d95342a9716651033e78d1
LunaDoro <dorothee.valentyn@gmail.com>
2024-10-06 18:06:55 +0200

    gestion des redirection en fonction du patient ou du medic

--- ProjetSymfonyPerso/src/Controller/AccueilController.php
+++ ProjetSymfonyPerso/src/Controller/AccueilController.php
@@ -11,6 +11,19 @@ class AccueilController extends AbstractController
     #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
+        // Vérification du rôle 'ROLE_PATIENT'
+        if ($this->isGranted('ROLE_PATIENT')) {
+            //creer page de vue patient avec exos?
+            return $this->redirectToRoute('');
+        }
+
+        // // Vérification du rôle 'ROLE_MEDIC'
+        // if ($this->isGranted('ROLE_MEDIC')) {
+        //     // Rediriger les médecins vers leur page d'accueil medecins avec les différents liens.
+        //     return $this->redirectToRoute('');
+        // }
+
+        // Si l'utilisateur n'a aucun rôle particulier, on affiche la page d'accueil générale
         return $this->render('accueil/index.html.twig');
     }
 }
