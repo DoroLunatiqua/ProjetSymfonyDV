3ddbce2feac30debaaaa825886b0add0fc983c0b
LunaDoro <dorothee.valentyn@gmail.com>
2024-11-06 16:40:47 +0100

    qq modif de CSS

--- ProjetSymfonyPerso/migrations/Version20241106110058.php
+++ ProjetSymfonyPerso/migrations/Version20241106110058.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241105104714 extends AbstractMigration
+final class Version20241106110058 extends AbstractMigration
 {
     public function getDescription(): string
     {
